531b5ecb448b2e875b715223d03be5c5ae77be7e
Marko Bogdanovic <mamaveb@gmail.com>
2016-10-18 15:37:56 +0200

    Listen for messages from publisher

--- PubSub/PubSub.cpp
+++ PubSub/PubSub.cpp
@@ -10,6 +10,96 @@ void InitializeWindowsSockets() {
 	}
 }
 
+void setNonBlockingMode(SOCKET * socket)
+{
+	// Set socket to nonblocking mode
+	unsigned long int nonBlockingMode = 1;
+	int iResult = ioctlsocket(*socket, FIONBIO, &nonBlockingMode);
+
+	if (iResult == SOCKET_ERROR)
+	{
+		printf("ioctlsocket failed with error: %ld\n", WSAGetLastError());
+		closesocket(*socket);
+		exit(EXIT_FAILURE);
+	}
+}
+
+int Select(SOCKET * socket)
+{
+	// Initialize select parameters
+	FD_SET set;
+	timeval timeVal;
+
+	FD_ZERO(&set);
+	// Add socket we will wait to read from
+	FD_SET(*socket, &set);
+
+	// Set timeouts to zero since we want select to return
+	// instantaneously
+	timeVal.tv_sec = 0;
+	timeVal.tv_usec = 0;
+
+	return 	select(0 /* ignored */, &set, NULL, NULL, &timeVal);
+}
+
+void waitForMessage(SOCKET * socket, unsigned buffer_size)
+{
+	int iResult;
+	char *recvbuf = (char*)malloc(buffer_size);
+	//set parameter for NonBlocking mode
+	setNonBlockingMode(socket);
+
+	printf("\n Waiting for messages...\n");
+	do
+	{
+		iResult = Select(socket);
+
+		// lets check if there was an error during select
+		if (iResult == SOCKET_ERROR)
+		{
+			fprintf(stderr, "select failed with error: %ld\n", WSAGetLastError());
+			continue;
+		}
+		// now, lets check if there are any sockets ready
+		if (iResult == 0)
+		{
+			// there are no ready sockets, sleep for a while and check again
+			Sleep(SERVER_SLEEP_TIME);
+			continue;
+		}
+
+		// Receive data until the client shuts down the connection
+		iResult = recv(*socket, recvbuf, buffer_size, 0);
+		if (iResult > 0)
+		{
+			char topic = recvbuf[0];
+			char message = recvbuf[1];
+			//bool success = pushMessage(topic, message)
+			//if(!success){
+			//createTopic(topic);
+			//pushMessage(topic,message);
+			//}
+			printf("Message received on topic %c: [%c]", topic, message);
+		}
+		else if (iResult == 0)
+		{
+			// connection was closed gracefully
+			printf("Connection with client closed.\n");
+			closesocket(*socket);
+			exit(EXIT_FAILURE);
+		}
+		else
+		{
+			// there was an error during recv
+			printf("recv failed with error: %d\n", WSAGetLastError());
+			closesocket(*socket);
+			exit(EXIT_FAILURE);
+		}
+		// here is where server shutdown loguc could be placed
+
+	} while (1);
+}
+
 DWORD WINAPI accept_publisher(LPVOID lpParam) {
 	SOCKET listenSocket = INVALID_SOCKET;
 	SOCKET* acceptedSocket;
@@ -23,7 +113,7 @@ DWORD WINAPI accept_publisher(LPVOID lpParam) {
 			memcpy(acceptedSocket, &socket, sizeof(SOCKET));
 			DWORD listen_publisher_id;
 			HANDLE listen_publisher_handle = CreateThread(NULL, 0, &listen_publisher, acceptedSocket, 0, &listen_publisher_id);
-			
+
 			// TODO: put SOCKET and HANDLER in container
 
 		}
@@ -33,7 +123,9 @@ DWORD WINAPI accept_publisher(LPVOID lpParam) {
 }
 
 DWORD WINAPI listen_publisher(LPVOID lpParam) {
-	//SOCKET
+	SOCKET* socket = (SOCKET*)lpParam;
+	waitForMessage(socket, DEFAULT_BUFLEN);
+	return 0;
 }
 
 void start_listening(SOCKET* listenSocket, char* port) {
--- PubSub/PubSub.h
+++ PubSub/PubSub.h
@@ -1,6 +1,10 @@
 #pragma once
 
+#define SERVER_SLEEP_TIME 50
+#define DEFAULT_BUFLEN 512
+
 void InitializeWindowsSockets();
 DWORD WINAPI accept_publisher(LPVOID);
 DWORD WINAPI listen_publisher(LPVOID lpParam);
 void start_listening(SOCKET* listenSocket, char* port);
+void waitForMessage(SOCKET*, unsigned);
