8ad601067350d0892b667841ccfe254276be7994
Marko Bogdanovic <mamaveb@gmail.com>
2016-10-18 18:08:59 +0200

    Refactor .h file and write skeleton for pushing on topic

--- PubSub/PubSub.h
+++ PubSub/PubSub.h
@@ -5,22 +5,43 @@
 #define SERVER_SLEEP_TIME 50
 #define DEFAULT_BUFLEN 512
 
-void InitializeWindowsSockets();
-DWORD WINAPI accept_publisher(LPVOID);
-DWORD WINAPI accept_subscriber(LPVOID);
-DWORD WINAPI listen_publisher(LPVOID lpParam);
-void start_listening(SOCKET* listenSocket, char* port);
-void waitForMessage(SOCKET*, unsigned);
-void setNonBlockingMode(SOCKET* socket);
-int Select(SOCKET* socket);
-bool push_socket_on_topic(List *topic_contents, SOCKET *socket, char topic);
-bool receiveTopic(SOCKET *socket, unsigned buffer_size, List *topic_contents);
-void free_topic_content(void * data);
-void free_socket(void * data);
-
 typedef struct _topic_content {
 	char topic;
 	List sockets;
 }TopicContent;
 
+typedef struct _param_struct {
+	SOCKET* socket;
+	List* topic_contents;
+}ParamStruct;
+
+#pragma region TOPIC_LIST
 TopicContent* list_find(List *list, char topic);
+void free_topic_content(void * data);
+void free_socket(void * data);
+#pragma endregion
+
+#pragma region THREAD_FUNCTIONS
+DWORD WINAPI accept_publisher(LPVOID);
+DWORD WINAPI accept_subscriber(LPVOID);
+DWORD WINAPI listen_publisher(LPVOID lpParam);
+DWORD WINAPI consume_messages(LPVOID lpParam);
+#pragma endregion
+
+#pragma region SOCKETS
+void InitializeWindowsSockets();
+void setNonBlockingMode(SOCKET* socket);
+int ready_for_receive(SOCKET* socket);
+void start_listening(SOCKET* listenSocket, char* port);
+#pragma endregion
+
+#pragma region PUBLISHER
+void waitForMessage(SOCKET*, unsigned, List*);
+void create_topic(List* topic_contents, char topic);
+bool pushMessage(char topic, char message, List* topic_contents);
+#pragma endregion
+
+#pragma region SUBSCRIBER
+bool push_socket_on_topic(List *topic_contents, SOCKET *socket, char topic);
+bool receiveTopic(SOCKET *socket, unsigned buffer_size, List *topic_contents);
+#pragma endregion
--- PubSub/main.cpp
+++ PubSub/main.cpp
@@ -12,29 +12,31 @@ void load_topics(List *topic_contents) {
 
 		list_append(topic_contents, &tc);
 	}
-
-
 }
 
 int main()
 {
+	InitializeWindowsSockets();
+
 	DWORD accept_publisher_id, accept_subscriber_id;
 	HANDLE accept_publisher_handle, accept_subscriber_handle;
 
-	InitializeWindowsSockets();
 	List topic_contents;
 	list_new(&topic_contents, sizeof(TopicContent), free_topic_content);
+	
+	//Create some initial topics
 	load_topics(&topic_contents);
 
 	accept_publisher_handle = CreateThread(NULL, 0, &accept_publisher, &topic_contents, 0, &accept_publisher_id);
 	accept_subscriber_handle = CreateThread(NULL, 0, &accept_subscriber, &topic_contents, 0, &accept_subscriber_id);
-	//nit(accept_publisher);
-	//nit(accept_subscriber);
+
 	getchar();
 
 	list_destroy(&topic_contents);
 	CloseHandle(accept_publisher_handle);
 	CloseHandle(accept_subscriber_handle);
+	
+	// TODO: Close all handles and sockets
 
 	return 0;
 }
