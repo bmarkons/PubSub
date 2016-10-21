3dbf20d9de54fa2e980de382ccf2e3dbb5658b21
Marko Bogdanovic <mamaveb@gmail.com>
2016-10-21 17:10:54 +0200

    Topic and messages as strings

--- Publisher/Publisher.h
+++ Publisher/Publisher.h
@@ -2,7 +2,11 @@
 
 #define SERVER_SLEEP_TIME 50
 #define HEADER_SIZE 1
-
+#define MAX_INPUT_SIZE 100
+typedef struct _string {
+	int length;
+	char* text;
+}TString;
 
 void InitializeWindowsSockets();
 void connectToServer(SOCKET* socket, char* ipv4_address, u_int port);
@@ -10,8 +14,8 @@ void disconnect(SOCKET*);
 void publishing_loop(SOCKET* socket);
 void input_message(char* message);
 void input_topic(char* topic);
-void publish(char message, char topic, SOCKET* socket);
-char* make_data_package(char message, char topic, int *data_size);
+void publish(char *message, char *topic, SOCKET* socket);
+char* make_data_package(char *message, char *topic, int *data_size);
 bool is_ready_for_send(SOCKET * socket);
 void set_nonblocking_mode(SOCKET * socket);
 bool send_nonblocking(SOCKET* socket, char* package, int data_size);
--- Subscriber/Subscriber.h
+++ Subscriber/Subscriber.h
@@ -5,16 +5,22 @@
 #define SERVER_SLEEP_TIME 50
 #define DEFAULT_BUFLEN 10
 #define HEADER_SIZE 1
+#define MAX_INPUT_SIZE 100
+
+typedef struct _string {
+	int length;
+	char* text;
+}TString;
 
 void InitializeWindowsSockets();
 void connectToServer(SOCKET* socket, char* ipv4_address, u_int port);
 void disconnect(SOCKET*);
 void input_message(char* message);
 void input_topic(char* topic);
-void subscribe(SOCKET* socket, char topic);
+void subscribe(SOCKET* socket, char *topic);
 bool send_nonblocking(SOCKET* socket, char* package, int data_size);
 bool send_all(SOCKET* socket, char *package, int data_size);
-char* make_data_package(char topic, int* data_size);
+char* make_data_package(char *topic, int* data_size);
 void wait_for_message(SOCKET* socket, unsigned buffer_size);
 void checkConfimation(SOCKET* socket);
 void set_nonblocking_mode(SOCKET* socket);
